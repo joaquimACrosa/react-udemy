2e927087ecfff93f366a88cab0280dec74d00afa
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-05-17 12:09:46 +0100

    Learning setState

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ class App extends Component {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
     ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was Clicked!');
+    // DON'T DO THIS: this.state.person[0].name = 'Joaquim';
+    this.setState(
+      {
+        persons: [
+          { name: 'Joaquim', age: 26 },
+          { name: 'Inês', age: 21 },
+        ]
+      }
+    )
   }
 
   render () {
     return (
       <div className="App">
         <h1>Hello React</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Coding in React</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
       </div> 
