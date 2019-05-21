8dd7d2368238f3fb8171eb39e7f30d13ea5f716c
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-05-21 04:17:32 +0100

    Introduction to React Hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
 
+  /*
+
+  CLASS BASED COMPONENT 
+  
   state = {
     persons: [
       { name: 'Max', age: 28 },
@@ -22,18 +26,45 @@ class App extends Component {
         ]
       }
     )
+  }*/
+
+  const [ personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+    ]
+  });
+
+  const [otherState, setOtherState] = useState('some other value');
+
+
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    //console.log('Was Clicked!');
+    // DON'T DO THIS: this.state.person[0].name = 'Joaquim';
+    setPersonsState(
+      {
+        persons: [
+          { name: 'Joaquim', age: 26 },
+          { name: 'Inês', age: 21 },
+        ]
+      }
+    )
+    setOtherState(
+      'some other value'
+    )
   }
 
-  render () {
     return (
       <div className="App">
         <h1>Hello React</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Coding in React</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Coding in React</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
       </div> 
     );
-  }
 }
 
-export default App;
+export default app;
