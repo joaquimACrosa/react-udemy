93ea1667f7e8a30c3ac4d3eefd1d4e836854b89e
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-05-24 02:40:26 +0100

    Used inline styling in React

--- src/App.js
+++ src/App.js
@@ -37,10 +37,21 @@ class App extends Component {
   }
 
   render () {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hello React</h1>
-        <button onClick={() => this.switchNameHandler('Joaquim Rosa')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Joaquim Rosa')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
