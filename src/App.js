d2eb011c0e8892d156529c42d6e0d59a2f4a8cbe
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-05-25 20:56:41 +0100

    Udemy Assignment 1 - Done

--- src/App.js
+++ src/App.js
@@ -1,67 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
 
- 
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-    ]
-  }
+      user: "Joaquim",
+      user2: "Inês"
+  };
 
-  switchNameHandler = (newName) => {
-    //console.log('Was Clicked!');
-    // DON'T DO THIS: this.state.person[0].name = 'Joaquim';
+  changeUsername = (event) => {
+    console.log(event.target.value);
     this.setState(
       {
-        persons: [
-          { name: newName, age: 26 },
-          { name: 'Inês', age: 21 },
-        ]
+        user: event.target.value
       }
-    )
+    );
   }
 
-  nameChangedHandler = (event) => {
-    this.setState(
-      {
-        persons: [
-          { name: event.target.value, age: 26 },
-          { name: 'Inês', age: 21 },
-        ]
-      }
-    )
-  }
 
   render () {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     return (
       <div className="App">
-        <h1>Hello React</h1>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler('Joaquim Rosa')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, 'Joe Rosa')}
-          changed={this.nameChangedHandler}>Coding in React
-        </Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}> 
-        </Person>
+        <h1>Assignment 1</h1>
+        <UserOutput username={this.state.user}>is the coder of this React app</UserOutput>
+        <UserInput change={this.changeUsername} username={this.state.user}></UserInput>
+        <UserOutput username={this.state.user2}>is a beautifull girl! ❤</UserOutput>
       </div> 
     );
   }
