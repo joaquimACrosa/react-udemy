55a41cec12d7f873b3661a2817721315d3fd4b8e
Joaquim Rosa <joaquimacrosa@outlook.com>
2019-06-12 23:23:07 +0100

    Assignment 2: Done

--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -4,8 +4,8 @@ import React from 'react';
 const charComponent = ( props ) => {
 
     return (
-        <div className="charComponent" style={props.style}>
-            <p>{props.children}</p>
+        <div className="charComponent" style={props.style} onClick={props.click}>
+            <p>{props.character}</p>
         </div>
     )
 };
